4024b6209f2ff972b81dfbf9ef58ac9a47d59633
aoskainer <52571512+aoskainer@users.noreply.github.com>
2024-01-13 18:23:52 +0900

    chore: DBからレコード取るとこまで作りました。

--- src/controllers/UserSelectController.ts
+++ src/controllers/UserSelectController.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { UserSelectForm } from "../forms";
+import { UserSelectService } from "../services";
 
-const UserSelectController = (req: Request, res: Response, next: NextFunction) => {
+const UserSelectController = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const userSelectService = new UserSelectService();
     const form: UserSelectForm = {
       id: parseInt(req.query.id as string),
     }
-    res.json(form);
+    const users = await userSelectService.execute(form);
+    res.json(users);
   } catch (error) {
     next(error);
   }
