2a4c5819dda5c2a1a7222e33f40d4c9c51094189
aoskainer <52571512+aoskainer@users.noreply.github.com>
2024-01-13 20:37:43 +0900

    chore: log4jsを導入しました。

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -1,10 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import logger from '../logger';
 
 const ErrorHandlingMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(
-    `[${new Date().toISOString().replace('T', ' ').replace('Z', '')}] `
-    + `Error: ${err.message}`
-  );
+  logger.error(err.message);
   res.status(500).json({ message: err.message });
 }
 
--- src/middleware/RequestLoggingMiddleware.ts
+++ src/middleware/RequestLoggingMiddleware.ts
@@ -1,18 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import logger from '../logger';
 
 const RequestLoggingMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log(
-    `[${new Date().toISOString().replace('T', ' ').replace('Z', '')}] `
-    + `Request: ${req.method} ${req.path} | `
-    + `Query: ${JSON.stringify(req.query)} | `
-    + `FormData: ${JSON.stringify(req.body)}`
-  );
+  logger.info(`Request: ${req.method} ${req.path} | Query: ${JSON.stringify(req.query)} | FormData: ${JSON.stringify(req.body)}`);
   next();
-  console.log(
-    `[${new Date().toISOString().replace('T', ' ').replace('Z', '')}] `
-    + `Request: ${req.method} ${req.path} | `
-    + `Status: ${res.statusCode}`
-  );
+  logger.info(`Request: ${req.method} ${req.path} | Status: ${res.statusCode}`);
 }
 
 export default RequestLoggingMiddleware;
